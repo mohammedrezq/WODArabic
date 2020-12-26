9c55868b1d55f91a24367f62f58a5f88bccbb1b7
Mohammed Rezq <mohammedrezq2000@gmail.com>
2020-12-26 20:53:32 +0200

    remove gatsby-plugin-offline configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,9 +31,9 @@ module.exports = {
     },
     {
       resolve: "gatsby-plugin-offline",
-      options: {
-        precachePages: ['/static/', '/src/pages/*', '/src/components/*', '/src/images/*','/src/templates/*'],
-      },
+      // options: {
+      //   precachePages: ['/static/', '/src/images/*'],
+      // },
     },
     // "gatsby-transformer-remark",
     {
